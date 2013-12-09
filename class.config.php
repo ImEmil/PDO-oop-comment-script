57ceb11650c221a91780a6effdb3c4d63a16ec93
ImEmil <emil_nilsson_96@live.se>
2013-12-09 23:44:19 +0100

    Update class.config.php

--- class.config.php
+++ class.config.php
@@ -3,9 +3,8 @@
   define("password", "test123");
 
   try {
-	  $s = new PDO('mysql:host=yourHOSThere;dbname=yourDBnameHERE', username, password);
-	  
-    $s->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  	$s = new PDO('mysql:host=yourHOSThere;dbname=yourDBnameHERE', username, password);
+  	$s->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     } catch (PDOException $e) {
     
