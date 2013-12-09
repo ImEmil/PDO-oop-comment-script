ee7702174b5e6af13387ce13dcbb4699e0fba75c
ImEmil <emil_nilsson_96@live.se>
2013-12-09 23:54:49 +0100

    Update class.config.php

--- class.config.php
+++ class.config.php
@@ -1,13 +1,14 @@
 <?php
+
   define("username", "root");
   define("password", "test123");
 
   try {
   	$s = new PDO('mysql:host=yourHOSThere;dbname=yourDBnameHERE', username, password);
-  	$s->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  	array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     
     } catch (PDOException $e) {
     
-    echo 'Couldn't connect to database > error -> ' . $e->getMessage();
+    echo 'Error __> ' . $e->getMessage();
     
   }
