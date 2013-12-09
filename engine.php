eafb4afcca36e80c4bd06d0788154c709d3c5ab3
ImEmil <emil_nilsson_96@live.se>
2013-12-10 00:30:25 +0100

    Create engine.php

---
+++ engine.php
@@ -0,0 +1,11 @@
+<?php
+/*
+
+                                                   +-+-+-+-+-+-+
+                                                   |I|m|E|m|i|l|
+                                                   +-+-+-+-+-+-+
+                                         
+*/
+  require('classes/class.config.php');
+  require('classes/class.comment_handler.php');
+  $s  = new C_HANDLER($s);
