422fc029bc9a669fe194a6a7ac282070c4e7970d
ImEmil <emil_nilsson_96@live.se>
2013-12-09 23:43:34 +0100

    Database connection

---
+++ class.config.php
@@ -0,0 +1,15 @@
+<?php
+  define("username", "root");
+  define("password", "test123");
+
+  try {
+	  $s = new PDO('mysql:host=yourHOSThere;dbname=yourDBnameHERE', username, password);
+	  
+    $s->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    } catch (PDOException $e) {
+    
+    echo 'Couldn't connect to database > error -> ' . $e->getMessage();
+    
+  }
+?>
