b96a6fc65d19f8ece9ae6621ba80dc29694fbfb9
ImEmil <emil_nilsson_96@live.se>
2013-12-09 23:49:43 +0100

    Update class.comment_handler.php

--- class.comment_handler.php
+++ class.comment_handler.php
@@ -6,5 +6,17 @@ class C_Handler {
   public function __construct($pdo) {
     $this->s = $pdo;
   }
+  
+  public function insertComment($username, $email, $comment) {
+    $s = $this->s->prepare("INSERT INTO `comments` (`username`, `email`, `comment`, `timestamp`) VALUES (?, ?, ?, ?) ");
+    
+    $s->bindValue(1, $username);
+    $s->bindValue(2, $email);
+    $s->bindValue(3, $comment);
+    $s->bindValue(4, time());
+    $s->execute();
+    
+    return true;
+	}
 
 }
