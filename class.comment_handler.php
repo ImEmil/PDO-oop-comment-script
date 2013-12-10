28225058614726fac5afb7949de5dbed8d6a5070
ImEmil <emil_nilsson_96@live.se>
2013-12-10 19:27:17 +0100

    Update class.comment_handler.php

--- class.comment_handler.php
+++ class.comment_handler.php
@@ -17,13 +17,11 @@ class C_Handler {
   public function insertComment($username, $email, $comment) {
     $values = array( $username, $email, $comment );
     $exc = $this->s->prepare("INSERT INTO `comments` (`username`, `email`, `comment`, `timestamp`) VALUES (?, ?, ?, NOW()")->execute($values);
-   
     return true;
   }
     
   public function displayComments() {
     $exc = $this->s->prepare("SELECT * FROM `comments` ORDER BY `timestamp` DESC")->execute();
-    
     return $exc->fetchAll();
   }
   
@@ -33,6 +31,10 @@ class C_Handler {
   
   public function c_null($string) {
     return(trim(is_null($string)) ? true : false);   
+  }
+  
+  public function __destruct() {
+    $this->s = null;
   }
     
     
