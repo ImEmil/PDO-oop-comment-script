9c19bdb85f5733310f516fd853529830779fd044
ImEmil <emil_nilsson_96@live.se>
2013-12-10 23:57:25 +0100

    Update _table.sql

--- _table.sql
+++ _table.sql
@@ -1,8 +1,9 @@
-  CREATE TABLE IF NOT EXISTS `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(700) NOT NULL,
   `username` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `comment` text NOT NULL,
-  `timetsamp` timestamp ,
+  `tstamp` varchar(150) NOT NULL,
+  `ip` text NOT NULL,
   PRIMARY KEY (`id`)
-  ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
